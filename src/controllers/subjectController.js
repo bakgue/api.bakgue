cc042cb9c219d217adf5f0f3db9ffe7a88afbb28
Novelier Webbelier <novelier.webbelier@gmail.com>
2022-03-13 15:25:50 +0900

    [BE] Feature implementation : Watch Subject

--- src/controllers/subjectController.js
+++ src/controllers/subjectController.js
@@ -6,6 +6,10 @@ import subjectsInfo from "../json/subject.json";
 
 const SUBJECT_PUG_PATH = BASE_PUG_PATH + "subject/";
 
+const toUppercaseOnlyFirstLetter = (str) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
 export const getSubject = (req, res) => {
   const subjectHomeObj = pageInfo.subject.home;
   const allSubject = subjectsInfo;
@@ -20,8 +24,36 @@ export const getSubject = (req, res) => {
 };
 
 export const watchSubject = (req, res) => {
+  const {
+    params: { subname },
+  } = req;
+
+  let subjectObj;
+
+  for (let i = 0; i < subjectsInfo.length; i++) {
+    const element = subjectsInfo[i];
+    if (String(element.englishName) === String(subname)) {
+      subjectObj = element;
+    }
+  }
+
+  if (!subjectObj) {
+    return res.render(BASE_PUG_PATH + "root/not-found", {
+      noShowTop: true,
+      pageTitle: "Not Found - 404",
+      paragraph: `"${subname}" 이라는 과목을 찾지 못했습니다`,
+      to: "/subject",
+      suggestionParagraph: "그냥 과목 홈페이지로 갈까요?",
+    });
+  }
+
+  subjectObj.englishName = toUppercaseOnlyFirstLetter(subjectObj.englishName);
+
   return res.render(SUBJECT_PUG_PATH + "watch", {
-    pageTitle: "Information of {Subject Name}",
+    noShowTop: true,
+    subtitle: `${subjectObj.englishName}`,
+    additionalDescription: `${subjectObj.name} 에 대한 정보입니다.`,
+    subjectObj,
   });
 };
 
