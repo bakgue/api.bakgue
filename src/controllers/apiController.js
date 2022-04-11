ab11c7988fec5c994c578aee117a8a24f0c146cf
Novelier Webbelier <novelier.webbelier@gmail.com>
2022-04-11 19:59:05 +0900

    [BE] Feature implementation : API

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -1,1 +1,20 @@
 import { STATUS_CODE } from "./rootController";
+
+export const postSaveAss = async (req, res) => {
+  console.log("Controller : postSaveAss");
+  return res.send({ a: "Hello" });
+};
+
+export const postCancelSaveAss = async (req, res) => {
+  console.log("Controller : postCancelSaveAss");
+  return res.sendStatus(STATUS_CODE.OK_CODE);
+};
+export const postCheckSaveAss = async (req, res) => {
+  console.log("Controller : postCheckSaveAss");
+  return res.sendStatus(STATUS_CODE.OK_CODE);
+};
+
+export const postAddView = async (req, res) => {
+  console.log("Controller : postAddView");
+  return res.sendStatus(STATUS_CODE.OK_CODE);
+};
--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -1,10 +1,31 @@
 import express from "express";
+import {
+  postAddView,
+  postCancelSaveAss,
+  postCheckSaveAss,
+  postSaveAss,
+} from "../controllers/apiController";
 import { protectorMiddleware } from "../middlewares";
 
 const apiRouter = express.Router();
 
-apiRouter.route("/assignment/save/:assname").all(protectorMiddleware);
-apiRouter.route("/assignment/save/:assname/cancel").all(protectorMiddleware);
-apiRouter.route("/assignment/view/:assname").all(protectorMiddleware);
+const assRouter = express.Router();
+const saveRouter = express.Router();
+
+apiRouter.use("/assignment", assRouter);
+assRouter.use("/save", saveRouter);
+
+saveRouter.route("/:assname").all(protectorMiddleware).post(postSaveAss);
+saveRouter
+  .route("/:assname/cancel")
+  .all(protectorMiddleware)
+  .post(postCancelSaveAss);
+
+saveRouter
+  .route("/:assname/check")
+  .all(protectorMiddleware)
+  .post(postCheckSaveAss);
+
+assRouter.route("/view/:assname").all(protectorMiddleware).post(postAddView);
 
 export default apiRouter;
