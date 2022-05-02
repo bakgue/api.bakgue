25fb284f94cd4448b5d7aa734a7da7e4f564e569
Novelier Webbelier <novelier.webbelier@gmail.com>
2022-05-03 00:43:30 +0900

    Delete Config

--- src/db.js
+++ src/db.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  majorityretryWrites: true
 });
 
 const db = mongoose.connection;
