8a7fdbbf2d22583fe21a626cc4a51f07b786197f
Novelier Webbelier <novelier.webbelier@gmail.com>
2022-05-03 00:42:01 +0900

    Fix Config of DB

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  majorityretryWrites=true
+  majorityretryWrites: true
 });
 
 const db = mongoose.connection;
