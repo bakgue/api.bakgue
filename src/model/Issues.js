56c33727cade053c0d963e6eb793134bd99198e4
Ethan Jeongwonchoi Garden <novelier.webbelier@gmail.com>
2022-05-12 21:34:16 +0900

    [BE] Create an Issue Model

---
+++ src/model/Issues.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const issueSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+    maxlength: 2000,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Student",
+    required: true,
+  },
+});
+
+const Issue = mongoose.model("Issue", issueSchema);
+
+export default Issue;
