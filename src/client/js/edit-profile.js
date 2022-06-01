49a636567c5a32a6398a5b9ba8ab55b38ebb63de
Novelier-Webbelier <novelier.webbelier@gmail.com>
2022-06-02 00:58:07 +0900

    [FE] Update client side of edit profile page

--- src/client/js/edit-profile.js
+++ src/client/js/edit-profile.js
@@ -13,6 +13,7 @@ const NONE_DISPLAY_KEY = "none-display";
 const CLICK_EVENT_KEY = "click";
 
 let mode = false;
+let OK = false;
 
 function toggleMode() {
 	if (mode) {
@@ -25,12 +26,17 @@ function toggleMode() {
 }
 
 async function saveInformation(username, bio) {
+	if (bio.length === 0) {
+		bio = "false";
+	}
+
 	const response = await fetch(`api/profile/edit/${username}/${bio}`, {
 		method: "POST",
 	});
 
 	if (response.status === 200) {
 		toggleMode();
+		location.reload();
 	}
 }
 
@@ -41,12 +47,25 @@ function toggleShowArea() {
 
 		cancelBtn.addEventListener(CLICK_EVENT_KEY, handleClickCancelBtn);
 		saveBtn.addEventListener(CLICK_EVENT_KEY, handleClickSaveBtn);
+		usernameInput.addEventListener("input", checkOk);
 	} else {
 		profileEdit.classList.add(NONE_DISPLAY_KEY);
 		profileShow.classList.remove(NONE_DISPLAY_KEY);
 
 		cancelBtn.removeEventListener(CLICK_EVENT_KEY, handleClickCancelBtn);
 		saveBtn.removeEventListener(CLICK_EVENT_KEY, handleClickSaveBtn);
+		usernameInput.removeEventListener("input", checkOk);
+	}
+}
+
+function checkOk(event) {
+	const value = event.target.value;
+	if (value.length === 0) {
+		OK = false;
+		saveBtn.disabled = true;
+	} else {
+		OK = true;
+		saveBtn.disabled = false;
 	}
 }
 
